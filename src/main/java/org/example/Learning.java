2364cdfcbc708b99468fa0033eafe168563ef358
RRam2020 <reecerram@Gmail.com>
2024-10-24 19:50:41 -0700

    LAB01_002

--- src/main/java/org/example/Learning.java
+++ src/main/java/org/example/Learning.java
@@ -1,7 +1,39 @@
 package org.example;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Learning {
 private String name;
 private int count;
-List<String> friends;
+List <String> friends;
+
+    public Learning() {
+    name = "TJ";
+    count = 1;
+    friends = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public List<String> getFriends() {
+        return friends;
+    }
+
+    public void setFriends(List<String> friends) {
+        this.friends = friends;
+    }
 }
